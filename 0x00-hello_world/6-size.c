d26df3bfc96425f1aa30fa44c8f044b9a14cee7d
Xristianah <chtowobola@gmail.com>
2022-09-13 12:09:27 -0700

    task 4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main - print to screen
+ *   *
+ *    * Return: always 0 (success)
+ *     */
+int main(void)
+{
+		char c;
+			int i;
+				long int a;
+					long long int b;
+						float f;
+
+							printf("Size of a char: %zu byte(s)\n", sizeof(c));
+								printf("Size of an int: %zu byte(s)\n", sizeof(i));
+									printf("Size of a long int: %zu byte(s)\n", sizeof(a));
+										printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
+											printf("Size of a float: %zu byte(s)\n", sizeof(f));
+												return (0);
+}
