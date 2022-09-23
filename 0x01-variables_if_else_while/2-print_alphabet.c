8201724eeb670a3c13d93f2671469635e5da3356
Xristianah <chtowobola@gmail.com>
2022-09-22 19:57:55 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
