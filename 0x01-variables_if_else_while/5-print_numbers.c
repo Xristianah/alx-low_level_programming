3d7141583f1a3b28fad379b162f5b8e28e64e996
Xristianah <chtowobola@gmail.com>
2022-09-22 20:10:58 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description: digit numbers of base 10 starting from 0
+ *    * Return: Always  0 (success)
+*/
+int main(void)
+{
+	int count = 0;
+	while (count < 10)
+		printf("%d", count++);
+	printf("\n");
+	return (0);
+}
