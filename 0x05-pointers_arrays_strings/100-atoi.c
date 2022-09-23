a829f57629c33bef7530bb04c670de9757c49b97
Xristianah <chtowobola@gmail.com>
2022-09-22 18:44:05 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,40 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *  * swap_int - swaps the values of two integers
- *   * @a: number tested
- *    * @b: number tested
- *     *
- *      * Return: Always 0.
+ *  * _atoi - converts a string to an integer.
+ *   * @s: pointer to string.
+ *    *
+ *     * Return: integer gotten.
 */
-void swap_int(int *a, int *b)
+int _atoi(char *s)
 {
-	int temp = 0;
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	int index, ind2;
+	unsigned int res;
+	int sign = 1;
+	char now;
+
+	index = 0;
+	res = 0;
+	while (*(s + index) != '\0')
+	{
+		now = *(s + index);
+		if (now == '-')
+		{
+			sign *= -1;
+		}
+		if (now >= '0' && now <= '9')
+		{
+			ind2 = index;
+			while (*(s + ind2) > 47 && *(s + ind2) < 58)
+			{
+				res = (res * 10) + *(s + ind2)-'0';
+				ind2++;
+			}
+			break;
+		}
+		index++;
+	}
+	if (sign < 0)
+		res *= sign;
+	return (res);
 }
