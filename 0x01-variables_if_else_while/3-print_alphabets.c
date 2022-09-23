c2b65b734869be4fcde9abafc720d5cf4a069c5b
Xristianah <chtowobola@gmail.com>
2022-09-22 20:01:13 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description: a program that prints the alphabet in lowercase.
+ *    * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char upper_ch = 'A';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (upper_ch = 'A'; upper_ch <= 'Z'; upper_ch++)
+		putchar(upper_ch);
+	putchar('\n');
+	return (0);
+}
